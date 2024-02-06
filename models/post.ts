85113b4dd1b3661f87eefc071465822eca19e6f0
Isabel Robleda <isabel.robleda@gmail.com>
2024-02-06 08:17:01 -0600

    creation of model and schema of posts

---
+++ models/post.ts
@@ -0,0 +1,19 @@
+import mongoose, { Schema, Document, Model} from "mongoose";
+
+export interface IPost extends Document {
+    userId: Schema.Types.ObjectId;
+    image: string;
+    description: string;
+}
+
+const PostSchema: Schema = new Schema({
+    userId: { type: Schema.Types.ObjectId, ref: "User", required: true},
+    image: { type: String },
+    description: { type: String }
+})
+
+
+
+const Post: Model<IPost> = mongoose.models.User || mongoose.model<IPost>("Post", PostSchema)
+
+export default Post

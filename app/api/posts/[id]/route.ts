7d60f53e690974b07739a8b64ea24689886532c7
vidishashah25 <vidisha.shah14@gmail.com>
2024-02-06 16:10:28 +0100

    Create & Read operation of post done

--- app/api/posts/[id]/route.ts
+++ app/api/posts/[id]/route.ts
@@ -1,1 +1,27 @@
 import connectMongoDB from "@/libs/db";
+import Post from "@/models/post";
+import  { Schema } from "mongoose";
+import { NextResponse, NextRequest } from "next/server";
+
+interface Postdata {
+    userId: Schema.Types.ObjectId;
+    newImage: string;
+    newDescription: string;
+}
+
+interface PostdataGet {
+    userId: Schema.Types.ObjectId;
+    image: string;
+    description: string;
+}
+
+export async function GET(req: NextRequest, {params}: {params: any}){
+    try{
+        const {id}: {id: string | null} = params;
+        await connectMongoDB();
+        const post: PostdataGet | null = await Post.findOne({_id: id});
+        return NextResponse.json({post});
+    } catch(error){ 
+        return NextResponse.json({error: 'Error fetching post'}, {status: 500})
+    }
+}
--- app/api/posts/route.ts
+++ app/api/posts/route.ts
@@ -1,1 +1,32 @@
 import connectMongoDB from "@/libs/db";
+import Post from "@/models/post";
+import  { Schema } from "mongoose";
+import { NextResponse, NextRequest } from "next/server";
+
+interface Postdata {
+    userId: Schema.Types.ObjectId;
+    image: string;
+    description: string;
+}
+
+export async function POST(req : NextRequest){
+    try{
+        const {userId, image, description} : Postdata = await req.json();
+        await connectMongoDB();
+        await Post.create({userId,image,description});
+        return NextResponse.json({message : 'Post created'}, {status: 201})
+    }catch(err)
+    {
+        return NextResponse.json({ error: 'Error creating Post', err }, { status: 500 });
+    }
+}
+
+export async function GET() {
+    try {
+      await connectMongoDB();
+      const posts: Postdata[] = await Post.find()
+      return NextResponse.json({posts})
+    } catch(err) {
+      return NextResponse.json({error: 'Error fetching posts',err}, {status: 500})
+    }
+  }

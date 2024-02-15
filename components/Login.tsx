404c6c6f9acac4b78df2614aea819419e06bda66
vidishashah25 <vidisha.shah14@gmail.com>
2024-02-15 14:51:28 +0100

    Login UI updated

--- components/Login.tsx
+++ components/Login.tsx
@@ -3,6 +3,8 @@ import React from 'react'
 import axios from 'axios'
 import { useState } from 'react'
 import { useRouter } from 'next/navigation'
+import logo from '../app/assets/Reddit-Logo-500x281.png'
+import Image from 'next/image'
 
 function Login() {
 
@@ -23,33 +25,39 @@ function Login() {
 
 
     return (
-        <section className="bg-gray-50 dark:bg-gray-900">
+        <section className="bg-zinc-800">
         <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-            <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+        <Image
+                src={logo}
+                width={200}
+                height={200}
+                alt="Picture of the author"
+            />
+            <div className="w-full rounded-lg shadow border md:mt-0 sm:max-w-md xl:p-0 bg-zinc-900 border-gray-700">
                 <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-                    <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-                        Sign in to your account
+                    <h1 className="text-xl font-bold leading-tight tracking-tight md:text-2xl text-white text-center">
+                        Sign In 
                     </h1>
                     <form className="space-y-4 md:space-y-6" action="#" onSubmit={handleLogin}>
                         <div>
-                            <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
+                            <label htmlFor="email" className="block mb-2 text-sm font-medium text-white">Your email</label>
                             <input 
                                 type="email" 
                                 name="email" 
                                 id="email" 
-                                className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
+                                className=" border border-gray-300 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
                                 placeholder="name@company.com" 
                                 value={email} 
                                 onChange={(e) => setEmail(e.target.value)} />
                         </div>
                         <div>
-                            <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+                            <label htmlFor="password" className="block mb-2 text-sm font-medium text-white">Password</label>
                             <input 
                                 type="password" 
                                 name="password" 
                                 id="password" 
                                 placeholder="••••••••" 
-                                className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
+                                className=" border border-gray-300 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
                                 value={password} 
                                 onChange={(e) => setPassword(e.target.value)}/>
                         </div>
@@ -58,9 +66,9 @@ function Login() {
                         </div>
                         <button 
                             type="submit" 
-                            className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800 border border-slate-50">
+                            className="w-full bg-orange-700 text-white  font-medium rounded-lg text-sm px-5 py-2.5 text-center bg-primary-600 hover:bg-primary-700 focus:ring-primary-800 ">
                                 Sign in</button>
-                        <p className="text-sm font-light text-gray-500 dark:text-gray-400">
+                        <p className="text-sm font-light text-gray-400 text-center">
                             Don’t have an account yet? <a href="#" className="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign up</a>
                         </p>
                     </form>
